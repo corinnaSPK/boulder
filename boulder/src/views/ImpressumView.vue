c6b113ad0a9f50b5e615d6574648a7df50c251dd
corinnaSPK <praya@gmx.de>
2024-10-27 11:48:00 +0100

    main wrapper fuer dsgv  imprint

--- boulder/src/views/ImpressumView.vue
+++ boulder/src/views/ImpressumView.vue
@@ -1,27 +1,29 @@
 <template>
-	<div class="impressum">
-		<h1>Impressum</h1>
-		<p>
-			Corinna Berrang<br />
-			P&uuml;sterberg 15<br />
-			59581 Warstein
-		</p>
-		<h2>Kontakt</h2>
-		<p>E-Mail: corinna.berrang@web.de</p>
-		<p>
-			Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a>
-		</p>
+	<main>
+		<div class="impressum">
+			<h1>Impressum</h1>
+			<p>
+				Corinna Berrang<br />
+				P&uuml;sterberg 15<br />
+				59581 Warstein
+			</p>
+			<h2>Kontakt</h2>
+			<p>E-Mail: corinna.berrang@web.de</p>
+			<p>
+				Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a>
+			</p>
 
-		<p>
-			Quelle Bild: Image by
-			<a
-				href="https://pixabay.com/photos/climb-climbing-hall-bouldering-3040060/"
-				>Diana</a
-			>
+			<p>
+				Quelle Bild: Image by
+				<a
+					href="https://pixabay.com/photos/climb-climbing-hall-bouldering-3040060/"
+					>Diana</a
+				>
 
-			from Pixabay
-		</p>
-	</div>
+				from Pixabay
+			</p>
+		</div>
+	</main>
 </template>
 
 <script setup></script>
